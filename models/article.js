4858a8b8c76c70f7c62070d09d93eae707182b27
nicpenmer <nicole.penmer@gmail.com>
2018-06-09 09:16:39 -0400

    updates

--- models/article.js
+++ models/article.js
@@ -30,3 +30,42 @@ var Article = mongoose.model("Article", ArticleSchema);
 
 // Export the Article model
 module.exports = Article;
+
+
+
+///////
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+	headline: {type: String, unique: true},
+	summary: String,
+	article: String,
+	byline: String,
+	img: {url: String, credit: String},
+	transcript: String,
+	notes: [
+	    {
+	      // Store ObjectIds in the array
+	      type: Schema.Types.ObjectId,
+	      // The ObjectIds will refer to the ids in the Note model
+	      ref: "Note"
+	    }
+	],
+	saved: Boolean
+});
+
+ArticleSchema.methods.saveArticle = function(){
+	this.saved = true;
+	return this.saved;
+};
+
+ArticleSchema.methods.removeSavedArticle = function(){
+	delete this.saved;
+};
+
+var Article = mongoose.model('Article', ArticleSchema);
+
+module.exports = Article;
--- models/note.js
+++ models/note.js
@@ -17,3 +17,24 @@ var Note = mongoose.model("Note", NoteSchema);
 
 // Export the Note model
 module.exports = Note;
+
+/////////////////
+
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Create article schema
+var NoteSchema = new Schema({
+	// `noteText` is required and of type String
+	noteText: {type: String, required: true},
+	// `date` defaults to Date.now and is of type Date
+	date: {type: Date, default: Date.now}
+});
+
+// Create Note model using NoteSchema
+var Note = mongoose.model('Note', NoteSchema);
+
+// Export the Note model
+module.exports = Note;
